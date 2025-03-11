14c7b9013bb90cdeb281b6cc7ed345bc48f114e3
Joseph <40335314+JRedOW@users.noreply.github.com>
2025-03-11 19:08:42 +0000

    Add Full Schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,7 +1,38 @@
-import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { integer, jsonb, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
     id: serial('id').primaryKey(),
-    email: varchar(),
-    password_hash: varchar()
+    email: varchar().notNull().unique(),
+    password_hash: varchar().notNull()
+});
+
+export const study_set = pgTable('study_set', {
+    id: serial('id').primaryKey(),
+    owner_id: integer()
+        .notNull()
+        .references(() => user.id),
+    data: jsonb().notNull()
+});
+
+export const set_results = pgTable('set_results', {
+    id: serial('id').primaryKey(),
+    study_set_id: integer()
+        .notNull()
+        .references(() => study_set.id),
+    user_id: integer()
+        .notNull()
+        .references(() => user.id),
+    results: jsonb().notNull()
+});
+
+export const study_session = pgTable('study_session', {
+    id: serial('id').primaryKey(),
+    study_set_id: integer()
+        .notNull()
+        .references(() => study_set.id),
+    user_id: integer()
+        .notNull()
+        .references(() => user.id),
+    history: jsonb().notNull(),
+    buddy_id: integer().notNull()
 });
